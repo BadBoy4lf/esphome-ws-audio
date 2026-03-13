f88ea7bc75acc5aae4140799d5e6eccbf791cb41
BadBoy4lf <BadBoy4lf@users.noreply.github.com>
2026-03-13 23:54:54 +0100

    Update ws_audio.h

--- components/ws_audio/ws_audio.h
+++ components/ws_audio/ws_audio.h
@@ -2,25 +2,24 @@
 #include "esphome.h"
 #include "esphome/components/websocket_client/websocket_client.h"
 
+namespace ws_audio {
+
 class WebSocketAudio : public esphome::Component {
  public:
   void setup() override {
     client_ = new esphome::websocket_client::WebSocketClient();
     client_->set_url("ws://192.168.177.1:1880/endpoint/audio");
     client_->set_reconnect(true);
-    ESP_LOGI("ws_audio", "WebSocketAudio setup completed");
   }
 
   void start_stream() {
     if (!client_) return;
-    ESP_LOGI("ws_audio", "Starting WebSocket stream…");
     client_->connect();
     active_ = true;
   }
 
   void stop_stream() {
     if (!client_) return;
-    ESP_LOGI("ws_audio", "Stopping WebSocket stream…");
     client_->close();
     active_ = false;
   }
@@ -36,3 +35,5 @@ class WebSocketAudio : public esphome::Component {
   esphome::websocket_client::WebSocketClient *client_{nullptr};
   bool active_{false};
 };
+
+}  // namespace ws_audio
