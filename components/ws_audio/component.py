ab467d2a4aee0e77c1d3068c8c5672d280589724
BadBoy4lf <BadBoy4lf@users.noreply.github.com>
2026-03-13 23:38:33 +0100

    Update component.py

--- components/ws_audio/component.py
+++ components/ws_audio/component.py
@@ -1,4 +1,14 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
 ws_audio_ns = cg.esphome_ns.namespace("ws_audio")
 WebSocketAudio = ws_audio_ns.class_("WebSocketAudio", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(WebSocketAudio),
+})
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
